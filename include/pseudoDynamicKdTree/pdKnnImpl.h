4471de948e702825ae6a6ca3f92eba1f13735687
michaelyhuang23 <michaelyhuang23@gmail.com>
2022-05-11 01:29:05 -0400

    update

--- include/pseudoDynamicKdTree/pdKnnImpl.h
+++ include/pseudoDynamicKdTree/pdKnnImpl.h
@@ -36,14 +36,13 @@ namespace pargeo::pdKdTree
   void knnRange(nodeT *tree, objT &q, double &radius, objT *&out);
 
   template <int dim, typename nodeT, typename objT>
-  void knnRangeHelper(nodeT *tree, objT &q, objT qMin, objT qMax, double &radius, objT *&out, int relation=-1);
+  void knnRangeHelper(nodeT *tree, objT &q, objT qMin, objT qMax, double &radius, objT *&out);
 
   template <int dim, typename nodeT, typename objT>
   void knnRangeHelper(nodeT *tree, objT &q, objT qMin, objT qMax, 
-                      double &radius, objT *&out, int relation)
+                      double &radius, objT *&out)
   {
-    if(relation == -1)
-      relation = tree->boxCompare(qMin, qMax, tree->getMin(), tree->getMax());
+    int  relation = tree->boxCompare(qMin, qMax, tree->getMin(), tree->getMax());
     if (relation == tree->boxExclude || tree->empty())
     {
       return;
