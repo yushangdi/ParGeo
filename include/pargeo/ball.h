14f2e83509ddd18075ea4ff2a25e819bf0367d61
Michael Huang <michaelyhuang23@gmail.com>
2022-10-07 00:08:52 -0400

    modify ball

--- include/pargeo/ball.h
+++ include/pargeo/ball.h
@@ -6,19 +6,9 @@
 #include <iomanip>
 #include "parlay/parallel.h"
 #include "parlay/primitives.h"
+#include "pargeo/atomics.h"
 
 namespace pargeo {
-
-  struct _empty {
-    int arr[0]; // todo this produces a struct of size 0 but seems dangerous, need to check
-    _empty(){}
-    
-    template<class T>
-    _empty(T x){}
-
-    friend bool operator<(_empty a, _empty b) {return true;}
-    friend bool operator==(_empty a, _empty b) {return true;}
-  };
 
   template <int _dim, class pointT>
   class _ball {
@@ -32,16 +22,32 @@ namespace pargeo {
   		return center.distSqr(p) <= radius*radius;
   	}
 
+  	inline void count_increase(int number){
+  		pargeo::write_add(&count, number);
+  	}
+
   	inline pointT pointClosestToCenter(pointT& center, pointT& pMin, pointT& pMax) {
 		pointT p;
-		for (int d = 0; d < dim; ++ d) {
+		for (int d = 0; d < _dim; ++ d) {
 			p[d] = std::max(pMin[d], std::min(center[d], pMax[d]));
+		}
+		return p;
+	}
+
+	inline pointT pointFarthestFromCenter(pointT center, pointT& pMin, pointT& pMax){
+		pointT p;
+		for (int d = 0; d < _dim; ++ d){
+			p[d] = (center[d]*2 < pMin[d]+pMax[d]) ? pMax[d] : pMin[d]; 
 		}
 		return p;
 	}
 
   	bool intersect_rect(pointT& minR, pointT& maxR){
   		return center.distSqr(pointClosestToCenter(center, minR, maxR)) < radius*radius;
+  	}
+
+  	bool contains_rect(pointT& minR, pointT& maxR){
+  		return center.distSqr(pointFarthestFromCenter(center, minR, maxR)) < radius*radius;
   	}
   };  
 }
